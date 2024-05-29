a53509a0cf70e732b267a922f1048dcf7fdeb63d
GirumZ <girum.zebe@gmail.com>
2024-05-29 23:14:38 +0300

    [Update]: Added type anotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,11 +29,10 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
-        """ Constructor method"""
-
-        self.fields = fields
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
 
     def format(self, record: logging.LogRecord) -> str:
         """ Formats the log message"""
