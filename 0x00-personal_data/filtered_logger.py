9155b7489c7ac9dda9196f32fc8de6722068538d
GirumZ <girum.zebe@gmail.com>
2024-05-29 22:27:47 +0300

    [Update]: Added main() function for entry to the program

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,88 @@
 #!/usr/bin/env python3
 """ module for the filter_datum function"""
 import re
+import logging
+import mysql.connector
+import os
+from mysql.connector import MySQLConnection
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ Returns the a log message obfuscated"""
 
     to_match = f"({'|'.join(fields)})=([^\\{separator}]*)"
-    return re.sub(to_match, lambda m: f"{m.group(1)}={redaction}", message)
+    return re.sub(to_match, lambda m: f" {m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """ Constructor method"""
+
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Formats the log message"""
+
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ Returns a logger object"""
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> MySQLConnection:
+    """ Returns a database connection"""
+
+    user = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
+
+    connection = mysql.connector.connect(
+            user=user,
+            password=password,
+            host=host,
+            database=db_name)
+
+    return connection
+
+
+def main():
+    """ Entry point"""
+
+    db = get_db()
+    logger = get_logger()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = cursor.column_names
+    for row in cursor:
+        message = "".join("{}={}; ".format(key, value) for key, value in zip(fields, row))
+        logger.info(message.strip())
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
