73930c8ae32e4c06e983a16ddb0bd4381ae711b7
GirumZ <girum.zebe@gmail.com>
2024-05-29 22:39:05 +0300

    [Create]: Password encription module

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module for password hashing"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Returns hashed password"""
+
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Checks if password is valid"""
+
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
