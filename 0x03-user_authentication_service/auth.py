4791a38d6777e4f8d05b127348d666af773c646e
GirumZ <girum.zebe@gmail.com>
2024-06-12 16:41:19 +0300

    [Update]: Added get_user_from_session method to auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,14 @@ class Auth:
             return s_id
         except NoResultFound:
             return
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ Gets user_id from session_id"""
+
+        if session_id is None:
+            return
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user.email
+        except NoResultFound:
+            return
