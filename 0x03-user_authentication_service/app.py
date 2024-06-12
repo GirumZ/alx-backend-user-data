7840b1016628a99353666f08bc4737563aaac25e
GirumZ <girum.zebe@gmail.com>
2024-06-12 17:00:54 +0300

    [Update]: corrected the if statement in the logout method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,10 +51,11 @@ def logout() -> str:
 
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    else:
         abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect("/")
 
 
 if __name__ == '__main__':
