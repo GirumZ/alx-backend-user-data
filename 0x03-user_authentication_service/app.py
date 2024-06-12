0ea5ffb2d28a64e6fd0683ad659c74fbabf5651e
GirumZ <girum.zebe@gmail.com>
2024-06-12 14:09:37 +0300

    [Update]: Added user end-point to the app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Flask application"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
@@ -11,6 +13,20 @@ def home():
     """ Home page"""
 
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def user() -> str:
+    """ posts a user"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
