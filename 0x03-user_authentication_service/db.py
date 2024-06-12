d2663f3ea8f84975318f942b30574c3ef82fc60a
GirumZ <girum.zebe@gmail.com>
2024-06-12 09:41:42 +0300

    [Bug fix]: Handled exceptions that might rise

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,9 +34,13 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds new user to database"""
 
-        session = self._session
-        new_user = User(email=email, hashed_password=hashed_password)
-        session.add(new_user)
-        session.commit()
-        # session.close()
+        try:
+            session = self._session
+            new_user = User(email=email, hashed_password=hashed_password)
+            session.add(new_user)
+            session.commit()
+            # session.close()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
